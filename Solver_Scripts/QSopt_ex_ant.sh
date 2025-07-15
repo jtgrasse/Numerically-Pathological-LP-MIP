5f2a4e2cd9f456f0b03e453ae642009ee0aa71a6
Joshua Grassel <jtgrasse@ncsul.edu>
2025-07-15 13:16:44 -0400

    added qsopt_ex stdout logging

--- Solver_Scripts/QSopt_ex_ant.sh
+++ Solver_Scripts/QSopt_ex_ant.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PROB_DIR="../../../Problem_Files/"
-SOL_DIR="../../../Solver_Results/QSopt_ex_ant/"
+PROB_DIR="../../../../Problems/"
+SOL_DIR="../../../../Solver_Results/QSopt_ex_ant/"
 for PROBLEM in $PROB_DIR*
 do
 	PROBLEM_NAME=$(basename "${PROBLEM%.*}")
 	echo "############## SOLVING $PROBLEM_NAME ##############"
 	echo $PROBLEM
 	echo $PROBLEM_NAME
-	esolver -O "${SOL_DIR}${PROBLEM_NAME}.sol" -b "${SOL_DIR}${PROBLEM_NAME}.bas" $PROBLEM
+	esolver -O "${SOL_DIR}${PROBLEM_NAME}.sol" -b "${SOL_DIR}${PROBLEM_NAME}.bas" $PROBLEM &> "${SOL_DIR}${PROBLEM_NAME}_stdout.log"
 done
