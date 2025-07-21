91d1ae27d400825e7f79937f75834286cd3c3813
Joshua Grassel <jtgrasse@ncsul.edu>
2025-07-20 20:43:48 -0400

    scratch work on numerical condition number

--- Code/scratch.py
+++ Code/scratch.py
@@ -1,33 +1,39 @@
 import numpy as np
 from prob_gen_helper_funcs import *
 
-p = 14
-n = 32
+def nPolyBowl_cond(n, p, k):
+    n_plus_epsilon = n + 10**(-p)
+    epsilon = n_plus_epsilon - n
+    if epsilon == 0:
+      print("epsilon cannot be represented for RHS=npe, n=%d, p=%d, k=%d" % (n, p, k))
+    else:
+        A = np.ones((n, n))
+        A = A + np.eye(n)*epsilon
 
-one_plus_epsilon = add_min_value(1, "sci", p)
-epsilon = one_plus_epsilon - 1
+        for i in range(2,k+1):
+            Atmp = np.ones((n, n))
+            Atmp = Atmp + np.eye(n)*i*epsilon
 
-sol = (one_plus_epsilon)/(n+epsilon)
+            A = np.vstack((A, Atmp))
 
-LHS1 = one_plus_epsilon
-RHS1 = one_plus_epsilon
+    tmp = np.zeros((n, n*(k-1)))
+    tmp = np.vstack((tmp, np.eye(n*(k-1))))
+    A = np.hstack((A, tmp))
+    # Calculate the condition numer
+    print(np.linalg.cond(A, p=1))
 
-LHS2 = 1+2*epsilon
-RHS2 = (one_plus_epsilon)*(n + 2*epsilon)/(n+epsilon)
+nPolyBowl_cond(n=3, p=3, k=3)
 
-LHS3 = 1+3*epsilon
-RHS3 = (one_plus_epsilon)*(n + 3*epsilon)/(n+epsilon)
-
-LHS4 = 1+4*epsilon
-RHS4 = (one_plus_epsilon)*(n + 4*epsilon)/(n+epsilon)
-
-print(epsilon)
-print(sol)
-print(LHS1)
-print(RHS1)
-print(LHS2)
-print(RHS2)
-print(LHS3)
-print(RHS3)
-print(LHS4)
-print(RHS4)
+# N = [3, 5, 10, 100, 500, 1000, 2000, 4000]
+# P = [8, 9, 10, 11, 12, 13, 14, 15, 16]
+# K = [2, 4, 6, 8]
+#
+# for n in N:
+#   for p in P:
+#     for k in K:
+#       nPolyBowl(n, p, k, "npe", "../Problem_Files/")
+#
+# for n in N:
+#   for p in P:
+#     for k in K:
+#       nPolyBowl(n, p, k, "1pe", "../Problem_Files/")
