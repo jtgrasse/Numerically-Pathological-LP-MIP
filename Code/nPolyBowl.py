70101069de12a20e7927534786756ea3c5f16b84
Joshua Grassel <jtgrasse@ncsul.edu>
2026-05-18 15:00:03 -0400

    Change epsilon to a power of 2. Split nPolyBowl generator functions into a floating point version and rational version.

--- Code/nPolyBowl.py
+++ Code/nPolyBowl.py
@@ -2,12 +2,17 @@ import numpy as np
 
 from prob_gen_helper_funcs import *
 
-def nPolyBowl(n, p, k, RHS, folder):
+def nPolyBowl_float(RHS, n, p, k, folder):
+  '''
+  Generates a nPolyBowl using the following parameters:
+  n: number of variables
+  p: the exponent of epsilon, where epsilon = 2^(-p)
+  k: number of blocks of constraints
+  '''
   if RHS == "npe":
     # This problem is of the form [(1 1^T) + diag(epsilon)][x] <= [1 (n+epsilon)]
     # The RHS of n+epsilon is the limiting factor on the written data
-    # n_plus_epsilon = add_min_value(n, "sci", p)
-    n_plus_epsilon = n + 10**(-p)
+    n_plus_epsilon = n + 2**(-p)
     epsilon = n_plus_epsilon - n
     if epsilon == 0:
       print("epsilon cannot be represented for RHS=npe, n=%d, p=%d, k=%d" % (n, p, k))
@@ -29,16 +34,9 @@ def nPolyBowl(n, p, k, RHS, folder):
         b = np.vstack((b, btmp))
 
       c = -np.ones((n,1))
-      write_mpsV2(A, b, c, 53, folder+"nPolyBowl_npe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
-      # Digits available in the 25 character limit MPS filetype
-      # {sign} + {ones digit} + {decimal point} + {18 digits after decimal} + {e} + {sign} + {2 digit exponent}
-      # = 1 + 1 + 1 + 18 + 1 + 1 + 2 = 25
+      write_mps_float(A, b, c, 60, folder+"nPolyBowl_float_npe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
   elif RHS == "1pe":
-    # This problem is of the form [(1 1^T) + diag(epsilon)][x] <= [1 (1+epsilon)]
-    # The value 1+epsilon is the limiting factor on the written data
-    # However, the final solution is (1+epsilon)/(n+epsilon) will become very small
-    # one_plus_epsilon = add_min_value(1, "sci", p)
-    one_plus_epsilon = 1 + 10**(-p)
+    one_plus_epsilon = 1 + 2**(-p)
     epsilon = one_plus_epsilon - 1
 
     if epsilon == 0:
@@ -60,20 +58,137 @@ def nPolyBowl(n, p, k, RHS, folder):
         b = np.vstack((b, btmp))
 
       c = -np.ones((n, 1))
-      write_mpsV2(A, b, c, 53, folder + "nPolyBowl_1pe_" + str(n) + "_" + str(p) + "_" + str(k) + ".mps")
+      write_mps_float(A, b, c, 60, folder + "nPolyBowl_float_1pe_" + str(n) + "_" + str(p) + "_" + str(k) + ".mps")
 
-# nPolyBowl(n=32, p=1, k=2, folder="../Problem_Files/")
+def nPolyBowl_rational(RHS, n, p, k, folder):
+  '''
+  Generates a nPolyBowl using the following parameters:
+  n: number of variables
+  p: the exponent of epsilon, where epsilon = 2^(-p)
+  k: number of blocks of constraints
+  '''
+  if RHS == "npe":
+    epsilon = [[],[]]
+    epsilon[0] = int(1)
+    epsilon[1] = int(2**p)
 
-N = [3, 10, 50, 100, 250, 500, 750, 1000, 2000]
-P = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-K = [2, 4, 6, 8]
+    # A is a matrix of 1s with the diagonl having epsilon added to it.
+    # A[i,i] = 1 + epsilon = 1 + 1/2^p = (2^p + 1)/2^p = epsilon[1]+1 / epsilon[1]
+    A = [[],[]]
+    A[0] = np.ones((n,n), dtype=int)
+    A[0] = A[0] + np.eye(n, dtype=int)*epsilon[1]
 
-for n in N:
-  for p in P:
-    for k in K:
-      nPolyBowl(n, p, k, "npe", "../Problem_Files/")
+    A[1] = np.ones((n,n), dtype=int)
+    for d in range(n): A[1][d,d] = epsilon[1]
 
-for n in N:
-  for p in P:
-    for k in K:
-      nPolyBowl(n, p, k, "1pe", "../Problem_Files/")
+    # B is a vector of n+epsilon
+    # n+epsilon = n + 1/2^p = (n*2^p + 1)/2^p = (n*epsilon[1] + 1) / epsilon[1]
+    # watch out for n*(2^p) overflowing
+    b = [[],[]]
+    b[0] = np.ones((n, 1), dtype=int)
+    b[0] = b[0]*(n*epsilon[1]+1)
+
+    b[1] = np.ones((n, 1), dtype=int)
+    b[1] = b[1]*epsilon[1]
+
+    for i in range(2,k+1):
+      # Atmp is a matrix of 1s with the diagonl having i*epsilon added to it.
+      # Atmp[i,i] = 1 + i*epsilon = 1 + i/2^p = (2^p + i)/2^p = (epsilon[1]+i) / epsilon[1]
+      Atmp = [[],[]]
+      Atmp[0] = np.ones((n,n), dtype=int)
+      for d in range(n): Atmp[0][d,d] = epsilon[1]+i
+
+      Atmp[1] = np.ones((n,n), dtype=int)
+      for d in range(n): Atmp[1][d,d] = epsilon[1]
+
+      # b is a vector of n + i*epsilon + tri_num(i-1)*epsilon
+      # n + i*epsilon + tri_num(i-1)*epsilon = n + i/2^p + tri_num(i-1)/2^p
+      # = (n*2^p + i + tri_num(i-1)) / 2^p = (n*epsilon[1] + i + tri_num(i-1)) / epsilon[1]
+      btmp = [[],[]]
+      btmp[0] = np.ones((n,1), dtype=int)
+      btmp[0] = btmp[0]*(n*epsilon[1] + i + tri_num(i-1))
+      btmp[1] = np.ones((n,1), dtype=int)
+      btmp[1] = btmp[1]*epsilon[1]
+
+      A[0] = np.vstack((A[0], Atmp[0]))
+      A[1] = np.vstack((A[1], Atmp[1]))
+      b[0] = np.vstack((b[0], btmp[0]))
+      b[1] = np.vstack((b[1], btmp[1]))
+
+    c = [[],[]]
+    c[0] = -np.ones((n,1), dtype=int)
+    c[1] = np.ones((n,1), dtype=int)
+    write_mps_rational(A, b, c, 53, folder+"nPolyBowl_rational_npe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
+  
+  elif RHS == "1pe":
+    epsilon = [[],[]]
+    epsilon[0] = int(1)
+    epsilon[1] = int(2**p)
+
+    # A is a matrix of 1s with the diagonl having epsilon added to it.
+    # A[i,i] = 1 + epsilon = 1 + 1/2^p = (2^p + 1)/2^p = epsilon[1]+1 / epsilon[1]
+    A = [[],[]]
+    A[0] = np.ones((n,n), dtype=int)
+    A[0] = A[0] + np.eye(n, dtype=int)*epsilon[1]
+
+    A[1] = np.ones((n,n), dtype=int)
+    for d in range(n): A[1][d,d] = epsilon[1]
+
+    # B is a vector of 1+epsilon
+    # 1+epsilon = 1 + 1/2^p = (2^p + 1)/2^p = (epsilon[1] + 1) / epsilon[1]
+    b = [[],[]]
+    b[0] = np.ones((n, 1), dtype=int)
+    b[0] = b[0]*(epsilon[1]+1)
+
+    b[1] = np.ones((n, 1), dtype=int)
+    b[1] = b[1]*epsilon[1]
+
+    for i in range(2,k+1):
+      # Atmp is a matrix of 1s with the diagonl having i*epsilon added to it.
+      # Atmp[i,i] = 1 + i*epsilon = 1 + i/2^p = (2^p + i)/2^p = (epsilon[1]+i) / epsilon[1]
+      Atmp = [[],[]]
+      Atmp[0] = np.ones((n,n), dtype=int)
+      for d in range(n): Atmp[0][d,d] = epsilon[1]+i
+
+      Atmp[1] = np.ones((n,n), dtype=int)
+      for d in range(n): Atmp[1][d,d] = epsilon[1]
+
+      # b is a vector of 1 + epsilon + tri_num(i-1)*epsilon
+      # 1 + epsilon + tri_num(i-1)*epsilon = 1 + 1/2^p + tri_num(i-1)/2^p
+      # = (2^p + 1 + tri_num(i-1)) / 2^p = (epsilon[1] + 1 + tri_num(i-1)) / epsilon[1]
+      btmp = [[],[]]
+      btmp[0] = np.ones((n,1), dtype=int)
+      btmp[0] = btmp[0]*(epsilon[1] + 1 + tri_num(i-1))
+      btmp[1] = np.ones((n,1), dtype=int)
+      btmp[1] = btmp[1]*epsilon[1]
+
+      A[0] = np.vstack((A[0], Atmp[0]))
+      A[1] = np.vstack((A[1], Atmp[1]))
+      b[0] = np.vstack((b[0], btmp[0]))
+      b[1] = np.vstack((b[1], btmp[1]))
+
+    c = [[],[]]
+    c[0] = -np.ones((n,1), dtype=int)
+    c[1] = np.ones((n,1), dtype=int)
+    write_mps_rational(A, b, c, folder+"nPolyBowl_rational_1pe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
+
+def nPolyBowl(format, RHS, n, p, k, folder):
+  if format == "float":
+    nPolyBowl_float(RHS, n, p, k, folder)
+  elif format == "rational":
+    nPolyBowl_rational(RHS, n, p, k, folder)
+  else:
+    print("format must be either 'float' or 'rational'")
+
+formats = ["float", "rational"]
+RHSs = ["npe", "1pe"]
+N = [3, 10]
+P = [60]
+K = [2, 4, 6]
+
+for format in formats:
+  for RHS in RHSs:
+    for n in N:
+      for p in P:
+        for k in K:
+          nPolyBowl(format, RHS, n, p, k, "Problem_Files/")
