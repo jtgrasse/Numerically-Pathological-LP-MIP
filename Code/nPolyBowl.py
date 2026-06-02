af452a49958c17db3f95172574c9990504682e20
Joshua Grassel <jtgrasse@ncsul.edu>
2026-06-02 16:42:49 -0400

    Remove old rational nPolyBowl generator and MPS writer

--- Code/nPolyBowl.py
+++ Code/nPolyBowl.py
@@ -65,118 +65,6 @@ def nPolyBowl_double(RHS, n, p, k, folder):
 
 def nPolyBowl_rational(RHS, n, p, k, folder):
   '''
-  Generates a nPolyBowl using the following parameters:
-  n: number of variables
-  p: the exponent of epsilon, where epsilon = 2^(-p)
-  k: number of blocks of constraints
-  '''
-  if RHS == "npe":
-    epsilon = [[],[]]
-    epsilon[0] = int(1)
-    epsilon[1] = int(2**p)
-
-    # A is a matrix of 1s with the diagonl having epsilon added to it.
-    # A[i,i] = 1 + epsilon = 1 + 1/2^p = (2^p + 1)/2^p = epsilon[1]+1 / epsilon[1]
-    A = [[],[]]
-    A[0] = np.ones((n,n), dtype=int)
-    A[0] = A[0] + np.eye(n, dtype=int)*epsilon[1]
-
-    A[1] = np.ones((n,n), dtype=int)
-    for d in range(n): A[1][d,d] = epsilon[1]
-
-    # B is a vector of n+epsilon
-    # n+epsilon = n + 1/2^p = (n*2^p + 1)/2^p = (n*epsilon[1] + 1) / epsilon[1]
-    # watch out for n*(2^p) overflowing
-    b = [[],[]]
-    b[0] = np.ones((n, 1), dtype=int)
-    b[0] = b[0]*(n*epsilon[1]+1)
-
-    b[1] = np.ones((n, 1), dtype=int)
-    b[1] = b[1]*epsilon[1]
-
-    for i in range(2,k+1):
-      # Atmp is a matrix of 1s with the diagonl having i*epsilon added to it.
-      # Atmp[i,i] = 1 + i*epsilon = 1 + i/2^p = (2^p + i)/2^p = (epsilon[1]+i) / epsilon[1]
-      Atmp = [[],[]]
-      Atmp[0] = np.ones((n,n), dtype=int)
-      for d in range(n): Atmp[0][d,d] = epsilon[1]+i
-
-      Atmp[1] = np.ones((n,n), dtype=int)
-      for d in range(n): Atmp[1][d,d] = epsilon[1]
-
-      # b is a vector of n + i*epsilon + tri_num(i-1)*epsilon
-      # n + i*epsilon + tri_num(i-1)*epsilon = n + i/2^p + tri_num(i-1)/2^p
-      # = (n*2^p + i + tri_num(i-1)) / 2^p = (n*epsilon[1] + i + tri_num(i-1)) / epsilon[1]
-      btmp = [[],[]]
-      btmp[0] = np.ones((n,1), dtype=int)
-      btmp[0] = btmp[0]*(n*epsilon[1] + i + tri_num(i-1))
-      btmp[1] = np.ones((n,1), dtype=int)
-      btmp[1] = btmp[1]*epsilon[1]
-
-      A[0] = np.vstack((A[0], Atmp[0]))
-      A[1] = np.vstack((A[1], Atmp[1]))
-      b[0] = np.vstack((b[0], btmp[0]))
-      b[1] = np.vstack((b[1], btmp[1]))
-
-    c = [[],[]]
-    c[0] = -np.ones((n,1), dtype=int)
-    c[1] = np.ones((n,1), dtype=int)
-    write_mps_rational(A, b, c, folder+"nPolyBowl_rational_npe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
-  
-  elif RHS == "1pe":
-    epsilon = [[],[]]
-    epsilon[0] = int(1)
-    epsilon[1] = int(2**p)
-
-    # A is a matrix of 1s with the diagonl having epsilon added to it.
-    # A[i,i] = 1 + epsilon = 1 + 1/2^p = (2^p + 1)/2^p = epsilon[1]+1 / epsilon[1]
-    A = [[],[]]
-    A[0] = np.ones((n,n), dtype=int)
-    A[0] = A[0] + np.eye(n, dtype=int)*epsilon[1]
-
-    A[1] = np.ones((n,n), dtype=int)
-    for d in range(n): A[1][d,d] = epsilon[1]
-
-    # B is a vector of 1+epsilon
-    # 1+epsilon = 1 + 1/2^p = (2^p + 1)/2^p = (epsilon[1] + 1) / epsilon[1]
-    b = [[],[]]
-    b[0] = np.ones((n, 1), dtype=int)
-    b[0] = b[0]*(epsilon[1]+1)
-
-    b[1] = np.ones((n, 1), dtype=int)
-    b[1] = b[1]*epsilon[1]
-
-    for i in range(2,k+1):
-      # Atmp is a matrix of 1s with the diagonl having i*epsilon added to it.
-      # Atmp[i,i] = 1 + i*epsilon = 1 + i/2^p = (2^p + i)/2^p = (epsilon[1]+i) / epsilon[1]
-      Atmp = [[],[]]
-      Atmp[0] = np.ones((n,n), dtype=int)
-      for d in range(n): Atmp[0][d,d] = epsilon[1]+i
-
-      Atmp[1] = np.ones((n,n), dtype=int)
-      for d in range(n): Atmp[1][d,d] = epsilon[1]
-
-      # b is a vector of 1 + epsilon + tri_num(i-1)*epsilon
-      # 1 + i*epsilon + tri_num(i-1)*epsilon = 1 + i/2^p + tri_num(i-1)/2^p
-      # = (2^p + i + tri_num(i-1)) / 2^p = (epsilon[1] + i + tri_num(i-1)) / epsilon[1]
-      btmp = [[],[]]
-      btmp[0] = np.ones((n,1), dtype=int)
-      btmp[0] = btmp[0]*(epsilon[1] + i + tri_num(i-1))
-      btmp[1] = np.ones((n,1), dtype=int)
-      btmp[1] = btmp[1]*epsilon[1]
-
-      A[0] = np.vstack((A[0], Atmp[0]))
-      A[1] = np.vstack((A[1], Atmp[1]))
-      b[0] = np.vstack((b[0], btmp[0]))
-      b[1] = np.vstack((b[1], btmp[1]))
-
-    c = [[],[]]
-    c[0] = -np.ones((n,1), dtype=int)
-    c[1] = np.ones((n,1), dtype=int)
-    write_mps_rational(A, b, c, folder+"nPolyBowl_rational_1pe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
-
-def nPolyBowl_integer_rational(RHS, n, p, k, folder):
-  '''
   Generates a nPolyBowl using infinite precision rationals via Python's Fraction class.
   Stores all coefficients as Fraction objects for exact arithmetic.
   
@@ -213,7 +101,7 @@ def nPolyBowl_integer_rational(RHS, n, p, k, folder):
     # c is objective vector of -1s
     c = [Fraction(-1) for _ in range(n)]
     
-    write_mps_integer_rational(A, b, c, folder+"nPolyBowl_integer_rational_npe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
+    write_mps_rational(A, b, c, folder+"nPolyBowl_rational_npe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
   
   elif RHS == "1pe":
     epsilon = Fraction(1, 2**p)
@@ -243,7 +131,7 @@ def nPolyBowl_integer_rational(RHS, n, p, k, folder):
     # c is objective vector of -1s
     c = [Fraction(-1) for _ in range(n)]
     
-    write_mps_integer_rational(A, b, c, folder+"nPolyBowl_integer_rational_1pe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
+    write_mps_rational(A, b, c, folder+"nPolyBowl_rational_1pe_"+str(n)+"_"+str(p)+"_"+str(k)+".mps")
 
 RHSs = ["npe", "1pe"]
 N = [10]
@@ -269,6 +157,6 @@ for RHS in RHSs:
   for n in N:
     for p in P:
       for k in K:
-        nPolyBowl_integer_rational(RHS, n, p, k, "Problem_Files/")
+        nPolyBowl_rational(RHS, n, p, k, "Problem_Files/")
 
 print("\nDone generating all problem instances!")
--- Code/prob_gen_helper_funcs.py
+++ Code/prob_gen_helper_funcs.py
@@ -57,60 +57,6 @@ def write_mps_double(A, b, c, p, filename="output.mps"):
 
 def write_mps_rational(A, b, c, filename="output.mps"):
     """
-    Write an LP problem in MPS format.
-    Assumes the Ax <= b and -inf < x < inf
-    Arguments:
-    A -- Coefficient matrix for the constraints. A[0] is the numerator and A[1] is the denominator
-    b -- Right-hand side vector. b[0] is the numerator and b[1] is the denominator
-    c -- Objective coefficients vector. c[0] is the numerator elements and c[1] is the denominator elements
-    p -- Precision of what is written to the file
-         specified as the number of digits after the decimal point.
-         Trailing 0s are dropped.
-    filename -- Name of the output MPS file.
-    """
-    num_constraints, num_vars = A[0].shape
-
-    # Open the file
-    with open(filename, "w") as f:
-        # Name section
-        f.write("NAME          LP_PROBLEM\n")
-
-        # Rows section
-        f.write("ROWS\n")
-        f.write(" N  OBJ\n")  # Objective row
-        for i in range(num_constraints):
-            f.write(f" L  R{i + 1}\n")  # Less-than constraint rows
-
-        # Columns section
-        f.write("COLUMNS\n")
-        for j in range(num_vars):
-            # Add the objective coefficient for this variable
-            f.write(f"    X{j + 1}    OBJ     {c[0][j, 0]:d}/{c[1][j, 0]}\n")
-
-            # Add each constraint coefficient for this variable
-            for i in range(num_constraints):
-                if A[0][i, j] != 0:  # Only non-zero entries are included
-                    f.write(f"    X{j + 1}    R{i + 1}   {A[0][i, j]}/{A[1][i, j]}\n")
-
-        # RHS section
-        f.write("RHS\n")
-        for i in range(num_constraints):
-            f.write(f"    RHS1    R{i + 1}   {b[0][i, 0]}/{b[1][i, 0]}\n")
-
-        # Bounds section (unrestricted variables, so none are specified)
-        f.write("BOUNDS\n")
-        for j in range(num_vars):
-            # Free Variables
-            f.write(f" FR BND1    X{j + 1}\n")  # "FR" denotes free/unrestricted
-            # Integers
-            # f.write(f" LI BND1    X{j+1}   -10\n")
-
-        # End the file
-        f.write("ENDATA\n")
-    print(f"MPS file '{filename}' has been written.")
-
-def write_mps_integer_rational(A, b, c, filename="output.mps"):
-    """
     Write an LP problem in MPS format using infinite-precision integers via Fraction objects.
     Assumes the Ax <= b and -inf < x < inf
     
