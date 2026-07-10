c97e2c12bfe9abc6f0d51b38a5a699dca697a3b1
Joshua Grassel <jtgrasse@ncsul.edu>
2026-07-10 16:48:35 -0400

    gurobi feasability checks

---
+++ Solvers/Gurobi_Check_Violations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PROBLEM_DIR="../Problem_Files/"
+SOL_DIR="../Solver_Results/Gurobi/"
+for SOLUTION in "$SOL_DIR"*.bas
+do
+	echo "############## CHECKING BASIS $SOLUTION ##############"
+	# extract the problem name from the solution file name
+	PROBLEM_NAME="${SOLUTION##*/}"
+	PROBLEM_NAME="${PROBLEM_NAME%_*}"
+	# make the violation log file name
+	VIOLATION_LOG="${SOLUTION%.bas}"
+	./QSopt_ex/build/check_violation/check_violation "$PROBLEM_DIR/$PROBLEM_NAME.mps" "$SOLUTION" &> "${VIOLATION_LOG}_violation.log"
+done
